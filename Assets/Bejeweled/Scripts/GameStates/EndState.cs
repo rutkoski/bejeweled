160259adbabb0cf57b32775b9a791ca4522c08fd
Rodrigo Rutkoski Rodrigues <rutkoski@gmail.com>
2021-01-22 10:28:01 -0300

    Display final score.

--- Assets/Bejeweled/Scripts/GameStates/EndState.cs
+++ Assets/Bejeweled/Scripts/GameStates/EndState.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 public class EndState : BaseCanvasState
 {
     [SerializeField] private Button m_restartButton;
+    [SerializeField] private Text m_scoreText;
 
     public override void Enter()
     {
         base.Enter();
 
         m_restartButton.onClick.AddListener(RestartButton_OnClick);
+
+        SetScore(GameController.Instance.Score);
     }
 
     public override void Exit()
@@ -23,5 +26,10 @@ public class EndState : BaseCanvasState
     private void RestartButton_OnClick()
     {
         GameController.Instance.RestartGame();
+    }
+
+    private void SetScore(int score)
+    {
+        m_scoreText.text = $"Score: {score}";
     }
 }
